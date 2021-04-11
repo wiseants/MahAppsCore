29d42fcbedaf7524fe06893aae1d3970648bb013
hcan <wiseant17@gmail.com>
2021-04-11 18:12:37 +0900

    뷰모델에 모듈매니저와 이벤트어그리케이트 참조 전달.

--- MahAppsCore/ViewModels/MainWindowViewModel.cs
+++ MahAppsCore/ViewModels/MainWindowViewModel.cs
@@ -1,19 +1,25 @@
-﻿using Prism.Mvvm;
+﻿using Prism.Events;
+using Prism.Modularity;
+using Prism.Mvvm;
 
 namespace MahAppsCore.ViewModels
 {
     public class MainWindowViewModel : BindableBase
     {
         private string _title = "Prism Application";
+        private IEventAggregator _eventAggregator;
+        private IModuleManager _moduleManager;
+
         public string Title
         {
             get { return _title; }
             set { SetProperty(ref _title, value); }
         }
 
-        public MainWindowViewModel()
+        public MainWindowViewModel(IEventAggregator eventAggregator, IModuleManager moduleManager)
         {
-
+            _eventAggregator = eventAggregator;
+            _moduleManager = moduleManager;
         }
     }
 }
