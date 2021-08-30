4f8af12fc78caff712ed46743bddbed41e9bc58d
hcan <wiseant17@gmail.com>
2021-08-30 20:17:56 +0900

    어플리케이션 툴 추가.

---
+++ MahAppsCore/Tools/AppTool.cs
@@ -0,0 +1,16 @@
+﻿using Prism.Ioc;
+using Prism.Unity;
+
+namespace MahAppsCore.Tools
+{
+    /// <summary>
+    /// 어플리케이션 툴.
+    /// </summary>
+    public class AppTool
+    {
+        /// <summary>
+        /// APP의 컨테이너.
+        /// </summary>
+        public static IContainerProvider Container => ((PrismApplication)System.Windows.Application.Current).Container;
+    }
+}
