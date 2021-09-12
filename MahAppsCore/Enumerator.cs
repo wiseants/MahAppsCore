1fa72308845896be93808f8a72e046efafa8d1c8
hcan <wiseant17@gmail.com>
2021-09-12 21:50:39 +0900

    셀렉터를 이용해서 컨트롤을 선택하는 예제 추가.

---
+++ MahAppsCore/Enumerator.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MahAppsCore
+{
+    public enum ParameterType
+    {
+        None,
+        Slider,
+        BitMask,
+        Selector
+    }
+}
---
+++ MahAppsCore/Selectors/ParameterTypeTemplateSelector.cs
@@ -0,0 +1,31 @@
+﻿// https://stackoverflow.com/questions/24916726/change-view-with-its-viewmodel-based-on-a-viewmodel-property/24918915#24918915
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace MahAppsCore.Selectors
+{
+    public class ParameterTypeTemplateSelector : DataTemplateSelector
+    {
+        public DataTemplate SliderViewTemplate
+        {
+            get;
+            set;
+        }
+
+        public DataTemplate BitMaskViewTemplate
+        {
+            get;
+            set;
+        }
+
+        public override DataTemplate SelectTemplate(object selector, DependencyObject container)
+        {
+            // selector 타입 확인해서 템플릿 선택.
+            return SliderViewTemplate;
+        }
+    }
+}
